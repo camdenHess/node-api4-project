14098a3b67472029b458a1be76bc5c771ec95b80
Camden Hess <camden.hess12@gmail.com>
2024-03-28 15:59:49 -0600

    added post functionality

--- index.js
+++ index.js
@@ -9,9 +9,38 @@ const PORT = process.env.PORT || 9000
 server.use(express.json())
 server.use(cors())
 
+const initialUsers = () => ([
+    {
+        username: 'ted',
+        password: 'mynameisted'
+    },
+    {
+        username: 'jeff',
+        password: 'jeffismyname'
+    },
+    {
+        username: 'mary',
+        password: 'marymaryhadalittlelamb'
+    }
+])
+
+let users = initialUsers()
 
 server.get('/api/users', (req, res, next) => {
-    res.json({message: 'more to do'})
+    res.status(200).json(users)
+})
+
+server.post('/api/register', (req, res, next) => {
+    const { username, password } = req.body
+    if (!username || !password) {
+        res.status(400).json({
+            message: 'username and password are required'
+        })
+    } else {
+        const newUser = {username: username, password: password}
+        users.push(newUser)
+        res.status(201).json(newUser)
+    }
 })
 
 server.use('*', (req, res) => {
